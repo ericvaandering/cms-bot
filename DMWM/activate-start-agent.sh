2966da4a0967aa8f1c0d9cf2c9701e4573fccc01
Eric Vaandering <ericvaandering@gmail.com>
2015-10-28 11:57:02 +0100

    Add init-agent

--- DMWM/activate-start-agent.sh
+++ DMWM/activate-start-agent.sh
@@ -6,3 +6,8 @@ $PWD/deploy/current/config/wmagent/manage activate-agent
 unlink deploy/current/sw*/var || /bin/true
 echo "Starting services"
 $PWD/deploy/current/config/wmagent/manage start-services
+
+if [[ $SQL_FLAVOR == "oracle" ]]; then
+  echo "Starting wmagent"
+  $PWD/deploy/current/config/wmagent/manage init-agent
+fi
